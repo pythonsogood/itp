5207d87cd6ac472f8300882340503c89847784e7
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2024-09-28 20:33:55 +0500

    Update 411.cpp

--- contester/week4/411.cpp
+++ contester/week4/411.cpp
@@ -4,13 +4,39 @@ int main(int argc, char *argv[]) {
     int n;
     std::cin >> n;
 
-    int a[n];
+    int a[n+1];
 
-    for (int i=1; i<=n; ++i) {
-        a[i-1] = i;
+    for (int i=0; i<=n; ++i) {
+        a[i] = i;
     }
 
-    int prime_numbers[];
+    int step = 2;
+    while (true) {
+        for (int i=0; i<=n; i+=step) {
+            a[i] = -1;
+        }
+        a[step] = step;
+
+        bool found = false;
+
+        for (int i=0; i<=n; ++i) {
+            if (a[i] != -1 && a[i] > step) {
+                step = a[i];
+                found = true;
+                break;
+            }
+        }
+
+        if (step >= n || !found) {
+            break;
+        }
+    }
+
+    for (int i=2; i<=n; ++i) {
+        if (a[i] != -1) {
+            std::cout << a[i] << " ";
+        }
+    }
 
     return 0;
 }
