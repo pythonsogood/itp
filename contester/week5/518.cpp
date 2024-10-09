4d6cf09920f01498ea1d1595f33de1ec25603caf
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2024-10-09 09:51:19 +0500

    518 WIP

--- contester/week5/518.cpp
+++ contester/week5/518.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 
-void cycle(int arr[], int length)
+int neighbors_count(int **matrix, int length, int pos_n, int pos_m) {
+    int count = 0;
+    int positions[8][2] = {
+        {pos_n-1, pos_m}, {pos_n-1, pos_m-1}, {pos_n, pos_m-1}, {pos_n-1, pos_m+1},
+        {pos_n+1, pos_m}, {pos_n+1, pos_m+1}, {pos_n, pos_m+1}, {pos_n+1, pos_m-1},
+    };
+    for (int i=0; i<8; ++i) {
+        int n = positions[i][0];
+        int m = positions[i][1];
+        if (n < 0 || m < 0 || n > length-1 || m > length-1) {
+            continue;
+        }
+        if (matrix[n][m] == 1) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void cycle(int matrix[], int length) {
+}
 
 int main(int argc, char *argv[]) {
-    
+    unsigned int n, t;
+
+    std::cin >> n >> t;
+
+    int *a[n][n];
+
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            std::cin >> a[i][j];
+        }
+    }
+
+    std::cout << neighbors_count(*a, n, 1, 0);
+
     return 0;
 }
