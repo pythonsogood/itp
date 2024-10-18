429e5749c8b33caeb5c113c276a574d14b62a6be
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2024-10-18 08:19:59 +0500

    Update 2.cpp

--- contester/week5/defense2/2.cpp
+++ contester/week5/defense2/2.cpp
@@ -4,9 +4,16 @@ int main() {
 	int n;
 	std::cin >> n;
 
+	int a[n][n];
 	for (int i=0; i<n; ++i) {
 		for (int j=0; j<n; ++j) {
-			std::cout << (i % 2 != j % 2 ? i+2 : 1);
+			a[i][j] = i % 2 != j % 2 ? i+2 : 1;
+		}
+	}
+
+	for (int i=0; i<n; ++i) {
+		for (int j=0; j<n; ++j) {
+			std::cout << a[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
