8f571bbc8d47bdc820f3b0bd360946da1ad9e56b
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2024-11-01 09:26:41 +0500

    814 ??

--- contester/week8/814.cpp
+++ contester/week8/814.cpp
@@ -1,47 +1,32 @@
+// Known issues:
+// CONTESTER DOES NOT GIVE NUMBER OF ROWS!!!
+
 #include <iostream>
 
-void move_to(int** x, int** y, char* direction, int length) {
-	switch (direction[0]) {
-		case 'N':
-		    int pos = *y + length;
-			*y = &pos;
-			break;
-
-		case 'S':
-		    int pos = *y - length;
-			*y = &pos;
-			break;
-
-		case 'E':
-            int pos = *x + length;
-			*x = &pos;
-            break;
-
-		case 'W':
-            int pos *x - length;
-			*x = &pos;
-            break;
-
-		default:
-			break;
+void move_to(int* xy, char* direction, int length) {
+	if (direction[0] == 'N') {
+ 		xy[1] += length;
+	} else if (direction[0] == 'S') {
+		xy[1] -= length;
+	} else if (direction[0] == 'E') {
+        xy[0] += length;
+    } else if (direction[0] == 'W') {
+        xy[0] -= length;
 	}
 }
 
 int main(int argc, char *argv[]) {
-	int* x = 0;
-	int* y = 0;
+	int xy[2] = {0, 0};
 
 	for (int i=0; i<3; ++i) {
-		char direction[5];
+		char direction[8];
 		int length;
-
 		std::cin >> direction >> length;
 
-		move_to(&x, &y, direction, length);
-		std::cout << *x << " " << *y << " " << length << std::endl;
+		move_to(xy, direction, length);
 	}
 
-	std::cout << *x << " " << *y;
+	std::cout << xy[0] << " " << xy[1];
 
     return 0;
 }
